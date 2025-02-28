e0c91d7cab39df03bdd01f4d23a3d541a5f0eb1c
Emil <emil88pl@gmail.com>
2025-02-28 11:35:54 +0000

    Added EnableScheduling

--- src/main/java/com/diditakeit/diditakeit/OverdueTaskScheduler.java
+++ src/main/java/com/diditakeit/diditakeit/OverdueTaskScheduler.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
-public class TaskScheduler {
+public class OverdueTaskScheduler {
 
     @Autowired
     private TaskRepository taskRepository;
